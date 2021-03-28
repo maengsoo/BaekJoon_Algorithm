9e95eabddefa81f48b04c74fc51c656b0e28208f
장명수 <dev.maeng@gmail.com>
2021-03-28 11:07:34 +0900

    add Q15552

---
+++ src/main/Q15552.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.Scanner;
+
+public class Q15552 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			arr[i] = a + b;
+			
+		}
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		sc.close();
+	}
+	//시간초과
+}
